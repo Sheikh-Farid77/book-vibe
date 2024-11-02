a14e62d0b88c6771d9caf980c963ed1194cd5a1f
Sheikh Farid <mdfaridsagor101@gmail.com>
2024-11-02 23:20:05 +0600

    added nav bar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root.jsx";
+import ErrorPage from "./Components/ErrorPage/ErrorPage.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 
 const router = createBrowserRouter([
- 
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard></Dashboard>
+      }
+    ]
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
